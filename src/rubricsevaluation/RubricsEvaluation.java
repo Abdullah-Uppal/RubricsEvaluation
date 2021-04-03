b686e699a1d51636d7593be39d1b537654a27bd8
Abdullah-Uppal <computerinsider4@gmail.com>
2021-04-03 19:41:50 +0500

    Update RubricsEvaluation.java

--- src/rubricsevaluation/RubricsEvaluation.java
+++ src/rubricsevaluation/RubricsEvaluation.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rubricsevaluation;
 
 /**
@@ -15,7 +10,6 @@ public class RubricsEvaluation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
