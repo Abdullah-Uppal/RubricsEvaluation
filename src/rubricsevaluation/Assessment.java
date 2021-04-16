d59e3e161df36b8685609b05282aa48e63d9ee82
Abdullah-Uppal <computerinsider4@gmail.com>
2021-04-16 10:50:23 +0500

    .

---
+++ src/rubricsevaluation/Assessment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubricsevaluation;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author abdullah
+ */
+public class Assessment {
+    private List<Question> questions;
+    private int totalMarks;
+    
+}
