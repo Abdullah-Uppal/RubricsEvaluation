8af01884bd01aca83426d27f37df920a9bf67183
Abdullah-Uppal <computerinsider4@gmail.com>
2021-04-16 15:03:23 +0500

    .

--- src/rubricsevaluation/Assessment.java
+++ src/rubricsevaluation/Assessment.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,5 +15,30 @@ import java.util.ArrayList;
 public class Assessment {
     private List<Question> questions;
     private int totalMarks;
+
+    public Assessment() {
+    }
+
+    public Assessment(List<Question> questions, int totalMarks) {
+        this.questions = questions;
+        this.totalMarks = totalMarks;
+    }
+    
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+    
     
 }
